bea356902d725c58fbc7857236ae2e0360fe6166
Jens Danbolt <jens_bolt92@hotmail.com>
2023-03-10 00:21:07 +0100

    Enabled function generate_random_move_orders to use any distribution function for quantity, by taking the distribution function to be used (and required arguments) as its own arguments.

--- sandbox.py
+++ sandbox.py
@@ -35,30 +35,35 @@ def wave_list_experiments(df: pd.DataFrame):
     print(f'df rows: {df.values.tolist()}')
 
 
-def check_CTP_plots(engine, date):
+def check_CTP_plots(engine, day):
     with Session(engine) as init_session:
         print_and_plot(init_session, ProjectionATP)
 
     # Execute five days
     for i in range(5):
         with Session(engine) as action_session:
-            execute_scheduled(action_session, date)
+            execute_scheduled(action_session, day)
             action_session.commit()
             # Check changes:
             # stockpoint_1501 = get_all(action_session, table=StockPoint)[1]
-            # print(f'Today is {date}. Stock is: {stockpoint_1501.current_stock}')
-        date += timedelta(days=1)
+            # print(f'Today is {day}. Stock is: {stockpoint_1501.current_stock}')
+        day += timedelta(days=1)
 
     with Session(engine) as end_session:
         print_and_plot(end_session, ProjectionCTP)
 
 
-def generate_random_move_orders(n, status, first_date, max_registration_delay, max_execution_delay, max_quantity):
+def generate_random_move_orders(n, status, first_date, max_registration_delay, max_execution_delay,
+                                quantity_distribution, *args, **rescale):
+    # Accepts random.distribution functions and its required *args.
+    # If the distribution by default returns values in its own range, such as 0 < x < 1, include rescale= at the end.
     orders = []
     for i in range(n):
         reg_date = first_date + timedelta(days=random.randint(0, max_registration_delay))
         exe_date = reg_date + timedelta(days=random.randint(0, max_execution_delay))
-        quantity = random.randint(max_quantity/3, max_quantity)
+        quantity = quantity_distribution(*args)
+        if rescale:
+            quantity = int(quantity * rescale['rescale'])
         orders.append(MoveOrder(quantity=quantity, date=exe_date, date_of_registration=reg_date, completion_status=status))
 
     return orders
@@ -80,8 +85,7 @@ if __name__ == "__main__":
         first_date = date.today() - timedelta(days=365)
 
         # Generate an order history
-        new_orders = generate_random_move_orders(n=3, status=1, first_date=first_date, max_registration_delay=350,
-                                                 max_execution_delay=8, max_quantity=60)
+        new_orders = generate_random_move_orders(3, 1, first_date, 350, 8, random.betavariate, 2, 5, rescale=60)
         route.move_orders += new_orders
         history_session.commit()
 
@@ -91,6 +95,6 @@ if __name__ == "__main__":
         completed_orders = history_session.scalars(stmt).all()
         for order in completed_orders:
             delivery_time = order.date - order.date_of_registration
-            print(delivery_time.days)
+            print(f'Delivery: {delivery_time.days} days. Quantity: {order.quantity} units.')
 
     reset_db(main_engine)
