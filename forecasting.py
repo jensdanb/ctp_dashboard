214a8955ef38fbcbd707cf03bc0b025b1c8043b0
Jens Danbolt <jens_bolt92@hotmail.com>
2023-03-11 13:44:54 +0100

    Refactored and given explanation.

--- forecasting.py
+++ forecasting.py
@@ -3,19 +3,34 @@ import random
 from database_model import *
 
 
-def generate_random_requests(n, status, first_date, max_registration_delay, expected_execution_time,
+def generate_random_requests(n, status, earliest_reg_date, last_reg_date, avg_requested_delivery_time,
                              quantity_distribution, *args, **rescale):
-    # Accepts random.distribution functions and its required *args.
-    # If the distribution by default returns values in its own range, such as 0 < x < 1, include rescale= at the end.
+    """
+    How the random components are made:
+
+    Date of registration is uniformly distributed between earliest and last date (from arguments).
+
+    Requested delivery time is beta distributed around the avg (from argument).
+    Beta distribution returns values between 0 and 1. To transform this into dates around the avg, it is:
+    Divided by the mean distribution output, thus moving the mean output to 1. Then Multiplied by avg_requested_delivery_time
+
+    Quantity distribution can be any random.distribution functions.
+    Pass the chosen distribution function as an argument, followed by any *args it needs.
+    If the distribution by default returns values in its own range, such as 0 < x < 1, include rescale= at the end.
+    """
     requests = []
+    reg_period = last_reg_date - earliest_reg_date  # print(type(reg_period), reg_period.days) -> [<class 'datetime.timedelta'>, 350]
+
     for i in range(n):
-        reg_date = first_date + timedelta(days=random.randint(0, max_registration_delay))
-        exe_time = timedelta(days=int(expected_execution_time*2*random.betavariate(2, 4)))
-        exe_date = reg_date + exe_time
+
+        reg_date = earliest_reg_date + timedelta(days=random.randint(0, reg_period.days))
+        a, b = 2, 4  # Alpha and Beta for the beta distribution
+        requested_delivery_time = timedelta(days=int(random.betavariate(a, b)) * ((a + b)/ a) * avg_requested_delivery_time)
+        requested_delivery_date = reg_date + requested_delivery_time
         quantity = quantity_distribution(*args)
         if rescale:
             quantity = int(quantity * rescale['rescale'])
-        requests.append(MoveRequest(date_of_registration=reg_date, expected_delivery_date=exe_date, quantity=quantity, quantity_delivered=quantity*status))
+        requests.append(MoveRequest(date_of_registration=reg_date, expected_delivery_date=requested_delivery_date, quantity=quantity, quantity_delivered=quantity*status))
 
     return requests
 
