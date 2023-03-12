c4fb69071111073fc773c99687b91474df033a2f
Jens Danbolt <jens_bolt92@hotmail.com>
2023-03-12 20:41:26 +0100

    WebApp is now operational (but barebones)

--- web_app.py
+++ web_app.py
@@ -1,14 +1,18 @@
-import pandas as pd
-
 from sandbox import *
+
 from typing import List
+import os
+import uuid
 from h2o_wave import Q, main, app, ui, data
 
 _id = 0
 ppath = '/home/jensd/PycharmProjects/ctp_dashboard'
 
 current_date = date.today()
-main_engine = create_engine("sqlite+pysqlite:///ctp_database.sqlite", echo=False, future=True)
+server_engine = create_engine("sqlite+pysqlite:///server_db.sqlite", echo=False, future=True)
+with Session(server_engine) as session:
+    premake_db(session, CcrpBase)
+
 # reset_db(engine)
 
 class TodoItem:
@@ -33,20 +37,52 @@ async def serve(q: Q):
     else:
         show_todos(q)
 
-    # Plot card
-    with Session(main_engine) as session:
-        premake_db(session, CcrpBase)
-        stockpoint_1501 = get_all(session, table=StockPoint)[1]
-        sub_df = ProjectionATP(session, stockpoint_1501).df.loc[:, ['demand', 'supply']].iloc[:30]
-    show_plot(q, sub_df)
+    # My WebApp starts here
+    if not q.client.initialized:
+        q.client.initialized = True
+
+        show_controls(q)
+        q.page['plot'] = ui.markdown_card(box=plot_box, title='Projected inventory', content='')
+
+    if q.args.make_plot:
+        with Session(server_engine) as plot_session:
+            stockpoint = get_all(plot_session, StockPoint)[1]
+            projection = ProjectionCTP(session, stockpoint)
+        await make_plot(q, projection.plot)
 
     await q.page.save()
 
 
 to_do_box = '1 1 2 3'
-plot_box = '1 4 5 6'
+plot_control_box = '3 1 2 3'
+plot_box = '1 4 7 4'
 
 
+def show_controls(q: Q):
+    q.page['controls'] = ui.form_card(
+        box=plot_control_box,
+        items=[
+            ui.text_xl("Lets make some plots"),
+            ui.button(name='make_plot', label='Make plot', primary=True)
+            # ui.slider(name='alpha', label='Alpha', min=5, max=100, step=1, value=q.client.alpha, trigger=True),
+        ]
+    )
+
+
+async def make_plot(q: Q, plot):
+    # Make temporary image file from the matplotlib plot
+    image_filename = f'{str(uuid.uuid4())}.png'
+    plot.savefig(image_filename)
+
+    # Upload to server and clean up
+    image_path, = await q.site.upload([image_filename])
+    os.remove(image_filename)
+
+    # Display:
+    q.page['plot'].content = f'![plot]({image_path})'
+
+
+# Not in use
 def show_plot(q: Q, df: pd.DataFrame):
     q.page['example'] = ui.plot_card(
         box=plot_box,
@@ -83,7 +119,7 @@ def show_todos(q: Q):
 
     # Display them
     q.page['to_do_card'] = ui.form_card(box=to_do_box, items=[
-        ui.text_l('To Do'),
+        ui.text_l('To Do (example card, not part of my app)'),
         ui.button(name='new_todo', label='New To Do...', primary=True),
         *not_done,
         *([ui.separator('Done')] if len(done) else []),
