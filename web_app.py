579c84acc2e1866222ccf292ffb3b3dbfcb95b73
Jens Danbolt <jens_bolt92@hotmail.com>
2023-03-31 15:44:33 +0200

    Push to test deployment

--- web_app.py
+++ web_app.py
@@ -11,6 +11,7 @@ from h2o_wave import Q, main, app, ui, data, copy_expando
 
 " deployment push"
 
+
 current_date = date.today()
 plotable_columns = ['demand', 'supply', 'inventory', 'ATP', 'CTP_route_1']
 column_plot_styles = {
