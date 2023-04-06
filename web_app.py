619524a7f32cf458620323b717a2631f93f884f4
Jens Danbolt <jens_bolt92@hotmail.com>
2023-04-06 23:19:08 +0200

    UI overhauled

--- web_app.py
+++ web_app.py
@@ -7,22 +7,21 @@ import pandas as pd
 from typing import List
 import os
 import uuid
-from h2o_wave import Q, main, app, ui, data, copy_expando
+from h2o_wave import site, Q, main, app, ui, data, copy_expando
 
 
 current_date = date.today()
-plotable_columns = ['demand', 'supply', 'inventory', 'ATP', 'CTP_route_1']
+plotable_columns = ['demand', 'supply', 'inventory', 'ATP', 'CTP']
 column_plot_styles = {
     'demand': {'color': 'red', 'type': 'interval'},
     'supply': {'color': 'blue', 'type': 'interval'},
     'inventory': {'color': 'teal', 'type': 'area'},
     'ATP': {'color': 'green', 'type': 'area'},
-    'CTP_route_1': {'color': 'orange', 'type': 'area'}
+    'CTP': {'color': 'orange', 'type': 'area'}
 }
 
 
-@app('/ctp', mode='multicast')
-async def serve_ctp(q: Q):
+def set_layout(q: Q):
     q.page['meta'] = ui.meta_card(box='', layouts=[
         ui.layout(
             breakpoint='xs',
@@ -37,40 +36,35 @@ async def serve_ctp(q: Q):
         )
     ])
 
-    """ Runs once, on startup """
+
+@app('/ctp', mode='multicast')
+async def serve_ctp(q: Q):
+    """ Run once, on startup """
+
     if not q.client.initialized:
+        set_layout(q)
         show_header(q)
         q.client.initialized = True
+
         q.user.db_engine = dbm.create_engine("sqlite+pysqlite:///:memory:", echo=False, future=True)
         dbm.reset_db(q.user.db_engine)
         with dbm.Session(q.user.db_engine) as init_session:
             dbm.add_from_class(init_session, CcrpBase)
             init_session.commit()
 
+            q.client.stockpoint = dbm.get_all(init_session, dbm.StockPoint)[1]
+            q.args.stockpoint_choice_group = str(q.client.stockpoint.id)
+
         q.client.plot_length = 12
+        q.client.plot_columns = plotable_columns
+
+        show_plot_stockpoint_chooser(q)
+        show_plot_controls(q)
+
+
 
     """ Rerun on user action """
 
-    q.page['header'] = ui.header_card(box='header_zone', title='My app', subtitle='Routing demonstration', items=[
-        ui.button(name='#page1', label='Page 1', link=True),
-        ui.button(name='#plot_page', label='Plotting', link=True),
-        ui.button(name='#page3', label='Page 3', link=True),
-    ])
-
-    # Remove previous pages.
-    del q.page['page1']
-    del q.page['plot_page']
-    del q.page['page3']
-
-    # Handle menu clicks.
-    if q.args['#'] == 'plot_page':
-        q.page['plot_page'] = ui.markdown_card(box='1 2 6 4', title='Plotting', content='Plotting content')
-    elif q.args['#'] == 'page3':
-        q.page['page3'] = ui.markdown_card(box='1 2 2 2', title='Page 3', content='Page 3 content')
-    else:
-        q.page['page1'] = ui.markdown_card(box='1 2 2 2', title='Page 1', content='Page 1 content')
-
-    stockpoint = dbm.run_with_session(q.user.db_engine, dbm.get_all, table=dbm.StockPoint)[1]
     copy_expando(q.args, q.client)
 
     if q.args.pre_fill_db:
@@ -78,39 +72,63 @@ async def serve_ctp(q: Q):
             dbm.add_from_class_if_db_is_empty(fill_session, CcrpBase)
 
     """ UI response on user action """
-    if q.args.matplotlib_plot_button:
+    if q.args.stockpoint_choice_group:
+        q.client.stockpoint = dbm.run_with_session(q.user.db_engine, dbm.get_by_id,
+                                                   table=dbm.StockPoint, element_id=int(q.client.stockpoint_choice_group))
+
         with dbm.Session(q.user.db_engine) as plot_session:
-            projection = ProjectionCTP(plot_session, stockpoint, plot_period=q.client.plot_length)
+            projection = ProjectionCTP(plot_session, q.client.stockpoint)
+        native_plot(q, projection, plot_period=q.client.plot_length)
+
+    elif q.args.matplotlib_plot_button:
+        with dbm.Session(q.user.db_engine) as plot_session:
+            projection = ProjectionCTP(plot_session, q.client.stockpoint, plot_period=q.client.plot_length)
         await mpl_plot(q, projection.plot)
+
     elif q.args.native_plot_button:
         with dbm.Session(q.user.db_engine) as plot_session:
-            projection = ProjectionCTP(plot_session, stockpoint)
+            projection = ProjectionCTP(plot_session, q.client.stockpoint)
         native_plot(q, projection, plot_period=q.client.plot_length)
+
     else:
-        show_db_controls(q)
+        show_plot_stockpoint_chooser(q)
         show_plot_controls(q)
 
     await q.page.save()
 
-db_box = '1 2 2 4'
-plot_control_box = '3 2 3 4'
-plot_box = '1 6 6 4'
-
 
 def show_header(q: Q):
     q.page['header'] = ui.header_card(box='header_zone', title='My app', subtitle='Routing demonstration', items=[
-        ui.button(name='#page1', label='Page 1', link=True),
-        ui.button(name='#page2', label='Page 2', link=True),
+        ui.button(name='#data_page', label='Data', link=True),
+        ui.button(name='#plot_page', label='Plotting', link=True),
         ui.button(name='#page3', label='Page 3', link=True),
     ])
 
 
+# Data page functions
 def show_db_controls(q: Q):
-    q.page['db_controls'] = ui.form_card(
+    q.page['data_page'] = ui.form_card(
         box='control_zone_left',
         items=[
             ui.text_xl("Database Controls"),
             ui.button(name='pre_fill_db', label='Prefill database')
+        ]
+    )
+
+
+# Plotting page functions
+def show_plot_stockpoint_chooser(q: Q):
+    valid_stockpoints = dbm.run_with_session(q.user.db_engine, dbm.get_all, table=dbm.StockPoint)
+    choices = [
+        ui.choice(str(stockpoint.id), stockpoint.name)
+    for stockpoint in valid_stockpoints]
+
+    q.page['stockpoint_chooser'] = ui.form_card(
+        box='control_zone_left',
+        items=[
+            ui.text_xl("Choose Stockpoint"),
+            ui.choice_group(name='stockpoint_choice_group', label='Pick one',
+                            value=q.args.stockpoint_choice_group, required=True, choices=choices)
         ]
     )
 
@@ -178,4 +196,3 @@ async def mpl_plot(q: Q, plot):
 
     # Display:
     q.page['plot'].content = f'![plot]({image_path})'
-
