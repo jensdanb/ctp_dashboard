6d63124f95b401d2624a914e7299a52fe953bfec
Jens Danbolt <jens_bolt92@hotmail.com>
2023-04-16 17:08:41 +0200

    Moved scripts for order history testing into functions outside __main__

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,6 @@
 """ Database engine is initializes upon import!!! """
+from pprint import pprint
+
 import pandas as pd
 import numpy as np
 import random
@@ -7,13 +9,6 @@ from database_model import *
 from premade_db_content import CcrpBase
 from stock_projection_2D import StockProjection, ProjectionATP, ProjectionCTP
 from forecasting import generate_random_requests
-
-
-def print_and_plot(session, projection_type): # projection_type can be ProjectionATP or ProjectionCTP
-    stockpoint_1501 = get_all(session, table=StockPoint)[1]
-    projection = projection_type(session, stockpoint_1501)
-    print(projection.df.head(13))
-    projection.plot.show()
 
 
 def wave_list_experiments(df: pd.DataFrame):
@@ -36,7 +31,14 @@ def wave_list_experiments(df: pd.DataFrame):
     print(f'df rows: {df.values.tolist()}')
 
 
-def check_CTP_plots(engine, day):
+def print_and_plot(session, projection_type): # projection_type can be ProjectionATP or ProjectionCTP
+    stockpoint_1501 = get_all(session, table=StockPoint)[1]
+    projection = projection_type(session, stockpoint_1501)
+    print(projection.df.head(13))
+    projection.plot.show()
+
+
+def check_ctp_plots(engine, day):
     with Session(engine) as init_session:
         print_and_plot(init_session, ProjectionATP)
 
@@ -54,16 +56,7 @@ def check_CTP_plots(engine, day):
         print_and_plot(end_session, ProjectionCTP)
 
 
-if __name__ == "__main__":
-    global_date = date.today()
-    sandbox_engine = create_engine("sqlite+pysqlite:///:memory:", echo=False, future=True)
-    Base.metadata.create_all(sandbox_engine)
-    with Session(sandbox_engine) as init_session:
-        add_from_class_if_db_is_empty(init_session, CcrpBase)
-
-    check_CTP_plots(sandbox_engine, global_date)
-    print(f'Today is {date.today()} and global_date is {global_date}') # See if global_date was mutated
-
+def fake_order_history():
     with Session(sandbox_engine) as requests_generation_session:
         # Setup
         route = get_all(requests_generation_session, SupplyRoute)[1] # Second route in the DB
@@ -75,10 +68,13 @@ if __name__ == "__main__":
         route.move_requests += new_requests
         requests_generation_session.commit()
 
+
+def inspect_order_history():
     with Session(sandbox_engine) as history_inspection_session:
         # Find the content made in previous session
         route = get_all(history_inspection_session, SupplyRoute)[1]
-        stmt = select(MoveRequest).where(MoveRequest.quantity_delivered == MoveRequest.quantity).where(MoveRequest.requested_delivery_date <= date.today())
+        stmt = select(MoveRequest).where(MoveRequest.quantity_delivered == MoveRequest.quantity).where(
+            MoveRequest.requested_delivery_date <= date.today())
         completed_orders = history_inspection_session.scalars(stmt).all()
 
         # Inspect order history
@@ -87,8 +83,18 @@ if __name__ == "__main__":
             delivery_time = request.requested_delivery_date - request.date_of_registration
             print(f'Delivery: {delivery_time.days} days. Quantity: {request.quantity} units.')
 
-        # Plot
 
+if __name__ == "__main__":
+    global_date = date.today()
+    sandbox_engine = create_engine("sqlite+pysqlite:///:memory:", echo=False, future=True)
+    Base.metadata.create_all(sandbox_engine)
+    with Session(sandbox_engine) as init_session:
+        add_from_class_if_db_is_empty(init_session, CcrpBase)
+
+    check_ctp_plots(sandbox_engine, global_date)
+    print(f'Today is {date.today()} and global_date is {global_date}') # See if global_date was mutated
+    fake_order_history()
+    inspect_order_history()
 
 
     reset_db(sandbox_engine)
