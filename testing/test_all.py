cc94eee3fee3ea021bbe148a87c5c4dc81ec65cd
Jens Danbolt <jens_bolt92@hotmail.com>
2023-04-27 12:30:43 +0200

    Spacing between test categories

--- testing/test_all.py
+++ testing/test_all.py
@@ -1,6 +1,8 @@
 import pytest
+
 from testing.test_database import *
 from testing.test_projections import *
+
 from testing.test_graphing import test_product_graph, test_graph_conversion
 from testing.test_graphing import test_html_result
 
