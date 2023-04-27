9d59c5f5c914af74ddf2401d47a1a3f4a73147b6
Jens Danbolt <jens_bolt92@hotmail.com>
2023-04-27 12:28:39 +0200

    Improved testing of graphs

--- testing/test_graphing.py
+++ testing/test_graphing.py
@@ -11,7 +11,7 @@ def test_product_graph():
 
     with dbm.Session(dbm.test_engine) as test_session:
         for product in dbm.get_all(test_session, dbm.Product):
-            graph = product_graph(test_session, product)
+            graph = product_to_graph(test_session, product)
 
             # Graph has correct content
             assert isinstance(graph, nx.DiGraph)
@@ -26,15 +26,23 @@ def test_product_graph():
 def test_graph_conversion():
     with dbm.Session(dbm.test_engine) as test_session:
         for product in dbm.get_all(test_session, dbm.Product):
-            graph = product_graph(test_session, product)
 
+            graph = product_to_graph(test_session, product)
             net = graph_to_net(graph)
             assert isinstance(net, Network)
 
-            # Commented out because they spam the browser with html pages. Uncomment to test
-            html_string = net_to_html_str(net)
-            assert isinstance(html_string, str)
 
-            # product_to_html_str does the above in one go
+def test_html_result():
+    with dbm.Session(dbm.test_engine) as test_session:
+        for product in dbm.get_all(test_session, dbm.Product):
+
+            graph = product_to_graph(test_session, product)
+            net = graph_to_net(graph)
+            html_string = net_to_html_str(net)
+
+            assert isinstance(html_string, str)
+            assert html_string[:6] == '<html>' and html_string[-6:] == '</html>'
+
+            # product_to_html_str() does all the above in one function
             assert product_to_html_str(test_session, product) == html_string
 
