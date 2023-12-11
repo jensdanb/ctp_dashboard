e5c9c02498d9f061d58a4a60ec8efc8b40083094
Jens Danbolt <jens_bolt92@hotmail.com>
2023-12-11 21:09:13 +0100

    Increased price

--- databasing/premade_db_content.py
+++ databasing/premade_db_content.py
@@ -10,7 +10,7 @@ class ProductA():
     def __init__(self):
         self.product = Product(
             name="Product A",
-            price=250,
+            price=85_00,
         )
         self.stock_points=[
                StockPoint(product=self.product, name="Unfinished goods", current_stock=630),
