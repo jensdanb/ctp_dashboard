b7ef2b8bed558eedaefec17cb3ab7e701b0e6b4d
Jens Danbolt <jens_bolt92@hotmail.com>
2023-12-11 01:37:35 +0100

    Redesigned page

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,6 +1,6 @@
 from databasing import database_model as dbm
 from projection import StockProjection, ProjectionCTP
-from pages.shared_content import get_selected, product_dropdown, stockpoint_choice_group
+from pages.shared_content import get_selected, DbContent, product_dropdown, stockpoint_choice_group
 
 import os
 import uuid
@@ -24,21 +24,25 @@ def layout(q: Q):
             breakpoint='xs',
             zones=[
                 ui.zone('header_zone'),  # DO NOT CHANGE header_zone WITHOUT ALSO CHANGING IT IN OTHER PAGES
-                ui.zone('inv_control_zone', direction=ui.ZoneDirection.COLUMN, zones=[
-                    ui.zone('inv_control_zone_a', size='42%'),
-                    ui.zone('inv_control_zone_b', size='58%'),
+                ui.zone('inv_sp_selection_zone'),
+                ui.zone('inv_status_zone', direction=ui.ZoneDirection.COLUMN, zones=[
+                    ui.zone('inv_status_zone_a', size='42%'),
+                    ui.zone('inv_status_zone_b', size='58%'),
                 ]),
+                ui.zone('inv_control_zone'),
                 ui.zone('plot_zone'),
             ]
         ),
         ui.layout(
             breakpoint='m',
             zones=[
-                ui.zone('header_zone'),
-                ui.zone('inv_control_zone', direction=ui.ZoneDirection.ROW, zones=[
-                    ui.zone('inv_control_zone_a', size='33%'),
-                    ui.zone('inv_control_zone_b', size='66%'),
+                ui.zone('header_zone'),  # DO NOT CHANGE header_zone WITHOUT ALSO CHANGING IT IN OTHER PAGES
+                ui.zone('inv_sp_selection_zone'),
+                ui.zone('inv_status_zone', direction=ui.ZoneDirection.ROW, zones=[
+                    ui.zone('inv_status_zone_a', size='33%'),
+                    ui.zone('inv_status_zone_b', size='66%'),
                 ]),
+                ui.zone('inv_control_zone'),
                 ui.zone('plot_zone'),
             ]
         )
@@ -46,6 +50,10 @@ def layout(q: Q):
 
 
 async def serve_inventory_page(q: Q):
+
+    with dbm.Session(q.user.db_engine) as fetching_session:
+        db_content = DbContent(q, fetching_session)
+
     if q.args.matplotlib_plot_button:
         with dbm.Session(q.user.db_engine) as plot_session:
             projection = project_plot_stockpoint_selection(q, plot_session)
@@ -56,12 +64,16 @@ async def serve_inventory_page(q: Q):
             projection = project_plot_stockpoint_selection(q, plot_session)
         native_plot(q, projection, plot_period=q.client.plot_length)
 
-    elif q.args.show_sp_move_orders:
-        await show_sp_move_orders(q)
-
     else:
-        show_plot_stockpoint_chooser(q, 'inv_control_zone_a', trigger1=True)
+        show_plot_stockpoint_chooser(q, 'inv_sp_selection_zone', trigger1=True)
+        await show_inventory_status(q, db_content, box='inv_status_zone_a')
+        await show_sp_move_orders(q, box='inv_status_zone_b')
         show_plot_controls(q)
+
+
+def fetch_inventory_data(q: Q):
+    with dbm.Session(q.user.db_engine) as fetching_session:
+        return
 
 
 def project_plot_stockpoint_selection(q, session, projection_class=ProjectionCTP):
@@ -69,29 +81,85 @@ def project_plot_stockpoint_selection(q, session, projection_class=ProjectionCTP
     return projection_class(session, stockpoint, plot_period=q.client.plot_length)
 
 
-def show_plot_stockpoint_chooser(q: Q, box, trigger1=True, trigger2=False):
+def show_plot_stockpoint_chooser(q: Q, box, trigger1=True, trigger2=True):
     with dbm.Session(q.user.db_engine) as session:
         product_chooser = product_dropdown(q, session, trigger=trigger1)
-        stockpoint_chooser = stockpoint_choice_group(q, session, trigger=trigger2)
+        stockpoint_chooser = stockpoint_choice_group(q, session, inline=True, trigger=trigger2)
 
     q.page['stockpoint_chooser'] = ui.form_card(
         box=box,
         items=[
+            ui.inline(items=[
             product_chooser,
             stockpoint_chooser
+            ])
         ]
+    )
+
+
+async def show_inventory_status(q: Q, db_content, box):
+
+    in_stock = db_content.current_stock
+    value_per_item = db_content.price / 100
+    stock_value = in_stock * value_per_item
+
+    q.page['inv_status'] = ui.tall_stats_card(
+        box=box,
+        items=[
+            ui.stat(label='Current Stock', value=f'{in_stock:,}'),
+            ui.stat(label='Unit value, $', value=f'{value_per_item:,.2f},-'),
+            ui.stat(label='Value of Stock, $', value=f'{stock_value:,.2f},-'),
+        ]
+    )
+
+
+async def show_sp_move_orders(q: Q, box):
+    # Get data from db
+    with dbm.Session(q.user.db_engine) as list_move_orders_session:
+        stockpoint = get_selected(q, list_move_orders_session, dbm.StockPoint)
+
+        incoming_moves = dbm.get_incoming_move_orders(list_move_orders_session, stockpoint)
+        outgoing_moves = dbm.get_outgoing_move_orders(list_move_orders_session, stockpoint)
+        pending_incoming = dbm.uncompleted_orders(incoming_moves)
+        pending_outgoing = dbm.uncompleted_orders(outgoing_moves)
+
+    # Convert to H2O Wave content
+    incoming_table = [
+        ui.stat_table_item(
+            label=str(order.id),
+            values=['incoming', str(order.quantity), order.order_date.isoformat(),
+                    str(order.completion_status), str(order.request_id)],
+            colors=['blue'] + ['black'] * 4
+        ) for order in pending_incoming
+    ]
+    outgoing_table = [
+        ui.stat_table_item(
+            label=str(order.id),
+            values=['outgoing', str(order.quantity), order.order_date.isoformat(),
+                    str(order.completion_status), str(order.request_id)],
+            colors=['red'] + ['black'] * 4
+        ) for order in pending_outgoing
+    ]
+
+    full_table = incoming_table + outgoing_table
+
+    # Show H2O Wave content
+    q.page['orders'] = ui.stat_table_card(
+        box=box,
+        title='Move Orders',
+        columns=['ID', 'incoming/outgoing', 'Quantity', 'Planned Date', 'Completion Status', 'Request ID'],
+        items=full_table
     )
 
 
 def show_plot_controls(q: Q):
     q.page['controls'] = ui.form_card(
-        box='inv_control_zone_b',
+        box='inv_control_zone',
         items=[
             ui.text_xl("Make Plot"),
             ui.buttons([
                 ui.button(name='matplotlib_plot_button', label='Plot with Matplotlib'),
-                ui.button(name='native_plot_button', label='Plot with WebApp Plot'),
-                ui.button(name='show_sp_move_orders', label='Show Incoming/Outgoing Orders')
+                ui.button(name='native_plot_button', label='Plot with WebApp Plot')
             ]),
 
             ui.slider(name='plot_length', label='Number of days in plot', min=10, max=50, step=1, value=q.client.plot_length),
@@ -156,40 +224,3 @@ async def mpl_plot(q: Q, plot):
     q.page['plot'].content = f'![plot]({image_path})'
 
 
-async def show_sp_move_orders(q: Q):
-    # Get data from db
-    with dbm.Session(q.user.db_engine) as list_move_orders_session:
-        stockpoint = get_selected(q, list_move_orders_session, dbm.StockPoint)
-
-        incoming_moves = dbm.get_incoming_move_orders(list_move_orders_session, stockpoint)
-        outgoing_moves = dbm.get_outgoing_move_orders(list_move_orders_session, stockpoint)
-        pending_incoming = dbm.uncompleted_orders(incoming_moves)
-        pending_outgoing = dbm.uncompleted_orders(outgoing_moves)
-
-    # Convert to H2O Wave content
-    incoming_table = [
-        ui.stat_table_item(
-            label=str(order.id),
-            values=['incoming', str(order.quantity), order.order_date.isoformat(),
-                    str(order.completion_status), str(order.request_id)],
-            colors=['blue'] + ['black'] * 4
-        ) for order in pending_incoming
-    ]
-    outgoing_table = [
-        ui.stat_table_item(
-            label=str(order.id),
-            values=['outgoing', str(order.quantity), order.order_date.isoformat(),
-                    str(order.completion_status), str(order.request_id)],
-            colors=['red'] + ['black'] * 4
-        ) for order in pending_outgoing
-    ]
-
-    full_table = incoming_table + outgoing_table
-
-    # Show H2O Wave content
-    q.page['plot'] = ui.stat_table_card(
-        box='plot_zone',
-        title='Move Orders',
-        columns=['ID', 'incoming/outgoing', 'Quantity', 'Planned Date', 'Completion Status', 'Request ID'],
-        items=full_table
-    )
